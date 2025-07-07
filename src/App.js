4bccee293f50966b90237c1d08e1681701434b09
Adrian <adrianshisia@gmail.com>
2025-07-08 00:35:41 +0300

    components and pages folder implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes,   Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Stocks from './pages/Stocks';
+import Layout from './components/Layout';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}/>
+      <Route path="/" element={<Home />}/>
+      <Route path="/stocks/:symbol" element={<Stocks />}/>
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
